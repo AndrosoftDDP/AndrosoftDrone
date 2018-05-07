6530d33cf248e230d168165c1c18b67128627e64
AndrosoftDDP <39050309+AndrosoftDDP@users.noreply.github.com>
2018-05-07 11:58:25 +0200

    Update soros_ir.ino

--- soros_ir.ino
+++ soros_ir.ino
@@ -1,3 +1,5 @@
+//ADDP 1.Etap - Written by AndrewF.K. in 2018
+//For more info please visit https://androsoft.yolasite.com/tech.php
 void setup() {
   DDRD = B11111110; //Legyen Pin 0 bemenet, Pin 1-7 kimenetek
   DDRD = DDRD | B11111100; //Allitsuk be: Pin 2-7 kimenet, TX es RX megvaltoztatasa nelkul
@@ -12,11 +14,11 @@ void loop() {
   PORTD = B00010000;  //Pin 4 jelszintje legyen magas
   delayMicroseconds(p); //p μs-ig
   PORTD = B00100000; //Pin 5 jelszintje legyen magas
-  delayMicroseconds(r); //p μs-ig
+  delayMicroseconds(r); //r μs-ig
   PORTD = B01000000; //Pin 6 jelszintje legyen magas
-  delayMicroseconds(y); //p μs-ig
+  delayMicroseconds(y); //y μs-ig
   PORTD = B10000000; //Pin 7 jelszintje legyen magas
-  delayMicroseconds(t); //p μs-ig
+  delayMicroseconds(t); //t μs-ig
   PORTD = B00000000; //Az osszes kimenet legyen alacsony jelszinten
   delayMicroseconds(16000 - (p + r + y + t)); //A kod 16 ms-onkent ismetlodik, igy lesz a jel frekvenciaja f = 62,5 1/s; 
 }
